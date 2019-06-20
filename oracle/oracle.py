cb4b6fab897a8bef4bfafa13f6d7d27731236a60
DEATHSTAR LOL <dylanbrophy@gmail.com>
2019-06-20 10:19:52 -0700

    fix for not enough funds

--- oracle/oracle.py
+++ oracle/oracle.py
@@ -2,7 +2,8 @@ from web3 import Web3, HTTPProvider, IPCProvider
 import time, requests, json
 
 # basic configuration
-contract_adr = "0xAc3a018B22677A52c4A5c5AabCA551898aBc49D1"
+contract_adr = "0x1574Ea34e90db0618c343E6E64a31e03c40383c9"
+etherbase = "0x37816524091AB70755f2B943fa194B3C407373e2"
 price_target = 1000.0 # in USD
 swing_amount = 5
 
@@ -51,11 +52,11 @@ print("Connecting contract ["+contract_adr+"]")
 
 contract = web3.eth.contract(address=contract_adr, abi=abi)
 
-transactor = contract.transact({'from':web3.eth.accounts[0]})
+transactor = contract.transact({'from':etherbase})
 caller = contract.call()
 
 def set_price(price_eth):
-    web3.personal.unlockAccount(web3.eth.accounts[0],password, 5) # give us 5 seconds to send the transaction
+    web3.personal.unlockAccount(etherbase,password, 5) # give us 5 seconds to send the transaction
     return transactor.setPrice(round(price_eth*10000))
 def get_price():
     return caller.base_price()/10000.0
@@ -76,15 +77,21 @@ while(True):
 
     if abs(token_price - price_target)>swing_amount:
         print("Changing token price from $", token_price, " to $", price_target, "...")
-        transaction = set_price(price_target/ether_price)
-        print("Transaction hash is ", hex(int.from_bytes(transaction, 'big')))
         try:
-            web3.eth.waitForTransactionReceipt(transaction, 1800) # up to 30 minutes
-            print("Complete.  New price is $", get_price()*get_etherprice())
-        except:
-            print("Transaction recept not received; transaction probably dropped.")
-            print("Trying again...")
-            ook()
+            transaction = set_price(price_target/ether_price)
+            print("Transaction hash is ", hex(int.from_bytes(transaction, 'big')))
+            try:
+                web3.eth.waitForTransactionReceipt(transaction, 1800) # up to 30 minutes
+                print("Complete.  New price is $", get_price()*get_etherprice())
+            except:
+                print("Transaction recept not received; transaction probably dropped.")
+                print("Trying again...")
+                ook()
+        except ValueError as e:
+            print(e.args[0]['message'])
+            print('recheck in 20 minutes')
+            smartsleep(60*20)
+            print("Token price is now $", get_price()*get_etherprice())
     else:
         smartsleep(180)
         print("Token price is now $", get_price()*get_etherprice())
